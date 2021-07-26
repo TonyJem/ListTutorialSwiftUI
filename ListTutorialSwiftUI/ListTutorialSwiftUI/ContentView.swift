28fb1bd98d271c4ee8d14dc1659a9de3f6922e5c
TonyJem <a.jemanov@gmail.com>
2021-07-26 22:06:41 +0300

    Update ContentView with starting code from tutorial

--- ListTutorialSwiftUI/ListTutorialSwiftUI/ContentView.swift
+++ ListTutorialSwiftUI/ListTutorialSwiftUI/ContentView.swift
@@ -1,10 +1,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let courseArray: [CourseObject] = [
+        CourseObject(courseName:
+                        "Профессия «Data Scientist»", aboutCourse: "Освойте самую востребованную профессию 2021 года!"),
+        CourseObject(courseName:
+                        "Специализация «Аналитик данных»", aboutCourse: "Полный курс по анализу данных с нуля до Data Analyst со специализацией в маркетинге или продуктовой аналитике"),
+        CourseObject(courseName:
+                        "Профессия «Fullstack-разработчик Python»", aboutCourse: "Освойте программирование на Python и Django и станьте фулстек-разработчиком"),
+        CourseObject(courseName:
+                        "Профессия «Автотестировщик на Python»", aboutCourse: "Быстрый вход в сферу IT, возможность удаленной работы"),
+        CourseObject(courseName:
+                        "Курс Data Science", aboutCourse: "Введение в Data Science"),
+        CourseObject(courseName:
+                        "Профессия «Аналитик Данных»", aboutCourse: "Базовый курс по анализу данных с нуля до Junior-специалиста")
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            
     }
+}
+
+
+struct CourseObject: Identifiable {
+    let id = UUID()
+    let courseName: String
+    let aboutCourse: String
 }
 
 struct ContentView_Previews: PreviewProvider {
